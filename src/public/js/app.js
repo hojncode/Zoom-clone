e8952e2f66899f5f09b48b61700d5e90317c1ee9
hojncode <glicm12@gmail.com>
2022-11-28 20:18:22 +0900

    home.pug : Add form,input / server.js : 어떤 브라우저에서도 동일하게 입력된 텍스트를 받을 수 있음 / app.js : server로 input태그에 입력된 값을 보내줌

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,5 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form")
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 function handleOpen() {
@@ -18,6 +20,17 @@ socket.addEventListener("message", handleMessage)
 
 socket.addEventListener("close", handleClose)
 
-setTimeout(() => {
-    socket.send("This message from the browser! - after 1sec")
-}, 1000);
+// setTimeout(() => {
+//     socket.send("This message from the browser! - after 1sec")
+// }, 1000);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    //socket.send -> sever로 ()안의 값을 보내줌.
+    socket.send(input.value);
+    //"" 비워줌.
+    input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
