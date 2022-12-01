dc86e6bd4e0205d2a3b62ea8d38e0c25af5b889c
hojncode <glicm12@gmail.com>
2022-12-02 03:28:03 +0900

    notification when user join or left

--- src/server.js
+++ src/server.js
@@ -23,8 +23,16 @@ wsServer.on("connection", (socket) => {
     socket.on("enter_room", (roomName, done) => {
         socket.join(roomName);
         done();
+        socket.to(roomName).emit("welcome");
     });
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+      });
+      socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", msg);
+        done();
 });
+})
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
